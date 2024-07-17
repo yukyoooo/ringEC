d8066d8492a7f1bd725ef79b4485ebc4be05b099
moriyama yukyo <moriyama-yukyo@alfos.jp>
2024-07-17 15:55:05 +0900

    news詳細作成

--- single.php
+++ single.php
@@ -1,12 +1,23 @@
 <?php get_header(); ?>
-
-	<main class="single-main">
-		<section class="single">
-			<?php while (have_posts()) : the_post(); ?>
-				<h2><?php the_title(); ?></h2>
-				<?php the_content(); ?>
-			<?php endwhile; ?>
-		</section>
-	</main>
-
-<?php get_footer(); ?>
+	<div class="single">
+		<div class="single-wrapper">
+			<div class="single-header">
+				<h2 class="single-header__title"><?php the_title(); ?></h2>
+			</div>
+			<div class="single-body">
+				<?php if ( have_posts() ) : ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+						<!-- 繰り返したい内容　ここから -->
+						<li class="single-entry">
+							<div class="single-entry__inner">
+								<?php the_content(); ?>
+							</div>
+						</li>
+						<!-- 繰り返したい内容　ここまで -->
+					<?php endwhile; ?>
+				<?php else : ?>
+					<p>まだ投稿はありません。</p>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
