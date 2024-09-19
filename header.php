7cb144780c803b79373bbaf94f3f42239c360a2c
moriyama yukyo <moriyama-yukyo@alfos.jp>
2024-09-19 22:48:10 +0900

    about追加

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 		<nav class="pc-nav">
 			<ul>
 				<li><a href="<?php bloginfo('url'); ?>">Home</a></li>
-<!--				<li><a href="#">About</a></li>-->
+				<li><a href="/about/">About</a></li>
 				<li><a href="/news/">News</a></li>
 			</ul>
 		</nav>
